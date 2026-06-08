8701ddb236ac6347077ee4d041e66464721528f5
Bastien <bastien.chevallier@laposte.net>
2026-06-08 10:51:30 +0200

    Added props spawning

--- Assets/_Water/Scripts/GameManager.cs
+++ Assets/_Water/Scripts/GameManager.cs
@@ -1,27 +1,40 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager Instance;
+
     [SerializeField] private float scrollSpeed = 10f;
 
     [Header("Props Spawner")]
+    [SerializeField] private Transform propsContainer;
     [SerializeField] private GameObject rockPrefab;
     [SerializeField] private GameObject coinPrefab;
 
     [SerializeField] private float timeBtwRock;
     [SerializeField] private float timeBtwCoin;
 
-    private float elapsedTimeRock;
+    private float elapsedTimeRock = .5f;
     private float elapsedTimeCoin;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [NonSerialized] public int Score;
 
+    private int PropsMultiplier => Mathf.Clamp(Score / 20, 1, 5);
+
+    private void Awake() => Instance = this;
+
+    private void Start()
+    {
+        for (int i = 0; i <= 5; i++)
+        {
+            Instantiate(rockPrefab, new(Random.Range(-4f, 4f), 1, 50 + i * 8f), transform.rotation, propsContainer);
+            Instantiate(coinPrefab, new(Random.Range(-4f, 4f), 1, 50 + i * 9f), transform.rotation, propsContainer);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.position -= Vector3.forward * (scrollSpeed * Time.deltaTime);
 
@@ -31,11 +44,19 @@ public class GameManager : MonoBehaviour
         if (elapsedTimeRock >= timeBtwRock)
         {
             elapsedTimeRock -= timeBtwRock;
+
+            //TODO Spawn rocks
+            for (int i = 0; i < PropsMultiplier; i++)
+                Instantiate(rockPrefab, new(Random.Range(-4f, 4f), 1, 100), transform.rotation, propsContainer);
         }
 
         if (elapsedTimeCoin >= timeBtwCoin)
         {
             elapsedTimeCoin -= timeBtwCoin;
+
+            //TODO Spawn coins
+            for (int i = 0; i < PropsMultiplier; i++)
+                Instantiate(coinPrefab, new(Random.Range(-4f, 4f), 1, 100), transform.rotation, propsContainer);
         }
     }
 }
---
+++ Assets/_Water/Scripts/Props.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+namespace com.IsartDigital.Water
+{
+    public class Props : MonoBehaviour
+    {
+        [SerializeField] private bool isCoin;
+
+        [SerializeField] private float maxDistance;
+
+        private void Update()
+        {
+            if (transform.position.z <= maxDistance)
+                Destroy(gameObject);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player"))
+                return;
+
+            if (isCoin)
+            {
+                Destroy(gameObject);
+                //TODO Add to score
+                GameManager.Instance.Score++;
+            }
+            else
+            {
+                Destroy(other.gameObject);
+                //TODO Kill player
+            }
+        }
+    }
+}
