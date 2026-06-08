9aa9958efeec9f680c73ff9247189d5f5f2450f4
Bastien <bastien.chevallier@laposte.net>
2026-06-08 10:04:45 +0200

    Ground movement

--- Assets/_Water/Scripts/GameManager.cs
+++ Assets/_Water/Scripts/GameManager.cs
@@ -2,15 +2,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private float scrollSpeed = 10f;
+
+    [Header("Props Spawner")]
+    [SerializeField] private GameObject rockPrefab;
+    [SerializeField] private GameObject coinPrefab;
+
+    [SerializeField] private float timeBtwRock;
+    [SerializeField] private float timeBtwCoin;
+
+    private float elapsedTimeRock;
+    private float elapsedTimeCoin;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position -= Vector3.forward * (scrollSpeed * Time.deltaTime);
+
+        elapsedTimeRock += Time.deltaTime;
+        elapsedTimeCoin += Time.deltaTime;
+
+        if (elapsedTimeRock >= timeBtwRock)
+        {
+            elapsedTimeRock -= timeBtwRock;
+        }
+
+        if (elapsedTimeCoin >= timeBtwCoin)
+        {
+            elapsedTimeCoin -= timeBtwCoin;
+        }
     }
 }
